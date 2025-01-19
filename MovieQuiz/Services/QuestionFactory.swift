1be558ccee9651c56ed3625318ee7e369f1088c6
Kirill Serebryannyy <sincerely.manny@gmail.com>
2025-01-19 19:42:15 +0300

    feat: add error handling for image loading in QuestionFactory

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -1,9 +1,21 @@
 import Foundation
+import UIKit
 
 final class QuestionFactory: QuestionFactoryProtocol {
   private let moviesLoader: MoviesLoading
   private weak var delegate: QuestionFactoryDelegate?
   private var movies: [MostPopularMovie] = []
+
+  private enum QuestionFactoryError: Error, LocalizedError {
+      case imageLoadingFailed
+    
+      var errorDescription: String? {
+          switch self {
+          case .imageLoadingFailed:
+              return NSLocalizedString("Error loading image", comment: "Error loading image")
+          }
+      }
+  }
 
   init(moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate?) {
     self.moviesLoader = moviesLoader
@@ -31,7 +43,7 @@ final class QuestionFactory: QuestionFactoryProtocol {
       loadData()
       return
     }
-    
+
     DispatchQueue.global().async { [weak self] in
       guard let self = self else { return }
       let index = (0..<self.movies.count).randomElement() ?? 0
@@ -43,28 +55,35 @@ final class QuestionFactory: QuestionFactoryProtocol {
       do {
         imageData = try Data(contentsOf: movie.resizedImageURL)
       } catch {
-        print("Failed to load image")
+        self.delegate?.didFailToLoadData(with: error)
+        return
       }
 
-      let rating = Float(movie.rating) ?? 0
-
-      var suggestedRating: Int
-      repeat {
-        suggestedRating = Int.random(in: 3...8)
-      } while Float(suggestedRating) == rating
-
-      let isMore = Int.random(in: 0...1) == 1
-      let text =
-        "Рейтинг этого фильма \(isMore ? "больше" : "меньше") чем \(suggestedRating)?"
-      let correctAnswer =
-        isMore
-        ? rating > Float(suggestedRating) : rating < Float(suggestedRating)
-
-      let question = QuizQuestion(
-        image: imageData,
-        text: text,
-        correctAnswer: correctAnswer)
       DispatchQueue.main.async { [weak self] in
+
+        if UIImage(data: imageData) == nil { // check wheher it's actually an image
+          self?.delegate?.didFailToLoadData(with: QuestionFactoryError.imageLoadingFailed)
+          return
+        }
+
+        let rating = Float(movie.rating) ?? 0
+
+        var suggestedRating: Int
+        repeat {
+          suggestedRating = Int.random(in: 3...8)
+        } while Float(suggestedRating) == rating
+
+        let isMore = Int.random(in: 0...1) == 1
+        let text =
+          "Рейтинг этого фильма \(isMore ? "больше" : "меньше") чем \(suggestedRating)?"
+        let correctAnswer =
+          isMore
+          ? rating > Float(suggestedRating) : rating < Float(suggestedRating)
+
+        let question = QuizQuestion(
+          image: imageData,
+          text: text,
+          correctAnswer: correctAnswer)
         guard let self = self else { return }
         self.delegate?.didReceiveNextQuestion(question: question)
       }
